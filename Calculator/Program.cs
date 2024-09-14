02bbe80dc25e4a90940ba3d057345ada5e6cdaff
sunnyayyl <sunny.ayyl@gmail.com>
2024-09-14 18:11:09 +0100

    Fix (now 100% functional #TODO: ()

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Calculator;
 
 namespace Calculator
@@ -23,7 +22,23 @@ namespace Calculator
             { Operators.Divide, 2 },
             { Operators.Power, 3 }
         };
+
+        public static Dictionary<Operators, Direction> AssociativeMap { get; } = new Dictionary<Operators, Direction>
+        {
+            { Operators.Plus, Direction.Left },
+            { Operators.Minus, Direction.Left },
+            { Operators.Multiply, Direction.Left },
+            { Operators.Divide, Direction.Left },
+            { Operators.Power, Direction.Right }
+        };
     }
+
+    public enum Direction
+    {
+        Left,
+        Right
+    }
+
     public enum Operators
     {
         Plus,
@@ -32,7 +47,7 @@ namespace Calculator
         Divide,
         Power,
     }
-    
+
     public enum TokenType
     {
         Invalid,
@@ -47,48 +62,92 @@ namespace Calculator
         public TValue Value { get; private set; } = value;
         public static bool operator ==(BetterEnum<TValue> a, BetterEnum<TValue> b) => a.Name == b.Name;
         public static bool operator !=(BetterEnum<TValue> a, BetterEnum<TValue> b) => a.Name != b.Name;
-        
     }
-    public class Token(TokenType tokenType, string tokenLiteral)
+
+    public interface Token
     {
-        public TokenType Type { get; private set; }=tokenType;
-        public string Literal { get; private set; }=tokenLiteral;
+        string Literal { get; }
+        TokenType Type { get; }
+    }
+
+    public class EOFToken : Token
+    {
+        public string Literal { get; private set; } = "\0";
+        public TokenType Type { get; private set; } = TokenType.EOF;
 
         public override string ToString()
         {
-            return "Token{Type = "+Type+", Literal = "+Literal+"}";
-        }
-
-        public Operators? GetOperator()
-        {
-            if (Global.OperatorsMap.TryGetValue(Literal, out var op))
-            {
-                return op;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public uint? GetPrecedence()
-        {
-            var op = this.GetOperator();
-            if (op != null)
-            {
-                return Global.PrecedenceMap[op.Value];
-            }
-            else
-            {
-                return null;
-            }
+            return "EOFToken{}";
         }
     }
+
+    public class InvalidToken : Token
+    {
+        public string Literal { get; private set; }
+        public TokenType Type { get; private set; } = TokenType.Invalid;
+
+        public InvalidToken(string literal)
+        {
+            this.Literal = literal;
+        }
+
+        public override string ToString()
+        {
+            return "InvalidToken{Literal=" + this.Literal + "}";
+        }
+    }
+
+    public class OperatorToken : Token
+    {
+        public string Literal { get; private set; }
+        public Operators Operator { get; private set; }
+        public uint Precedence { get; private set; }
+        public Direction Associative { get; private set; }
+        public TokenType Type { get; private set; } = TokenType.Operator;
+
+        public OperatorToken(string tokenLiteral)
+        {
+            this.Literal = tokenLiteral;
+            this.Operator = Global.OperatorsMap[this.Literal];
+            this.Precedence = Global.PrecedenceMap[this.Operator];
+            this.Associative = Global.AssociativeMap[this.Operator];
+        }
+
+        public override string ToString()
+        {
+            return $"OperatorToken{{Operator={this.Operator.ToString()}, Precedence={this.Precedence.ToString()}}}";
+        }
+    }
+
+    public class NumberToken : Token
+    {
+        public string Literal { get; private set; }
+        public int Value { get; private set; }
+        public TokenType Type { get; private set; } = TokenType.Number;
+
+        public NumberToken(string tokenLiteral)
+        {
+            this.Literal = tokenLiteral;
+            this.Value = int.Parse(tokenLiteral);
+        }
+
+        public NumberToken(int tokenValue)
+        {
+            this.Literal = tokenValue.ToString();
+            this.Value = tokenValue;
+        }
+
+        public override string ToString()
+        {
+            return "NumberToken{Literal = " + Literal + "}";
+        }
+    }
+
     public class Lexer // FIXME: Cannot be generalized as there is neither Union or a interface Indexable type? 
     {
         public string Input { get; private set; }
         public char CurrentCharater { get; private set; }
-        public int CurrentIndex {get; private set;}
+        public int CurrentIndex { get; private set; }
 
         public Lexer(string input)
         {
@@ -99,10 +158,10 @@ namespace Calculator
 
         public bool NextToken()
         {
-            if (this.CurrentIndex+1 < this.Input.Length)
+            if (this.CurrentIndex + 1 < this.Input.Length)
             {
                 this.CurrentIndex++;
-                this.CurrentCharater=this.Input[this.CurrentIndex];
+                this.CurrentCharater = this.Input[this.CurrentIndex];
                 return true;
             }
             else
@@ -118,32 +177,34 @@ namespace Calculator
             if (this.CurrentIndex + 1 < this.Input.Length)
             {
                 return this.Input[this.CurrentIndex + 1];
-            }else
+            }
+            else
             {
                 return '\0';
             }
         }
 
-        public bool Is(TokenType tt, char obj='\0')
+        public bool Is(TokenType tt, char obj = '\0')
         {
             var compared = (obj == '\0' ? this.CurrentCharater : obj).ToString();
             if (compared == "\0")
             {
                 return false;
             }
-             else if (int.TryParse(compared, out _))
+            else if (int.TryParse(compared, out _))
             {
                 return tt == TokenType.Number;
             }
             else if (Global.OperatorsMap.ContainsKey(compared))
             {
-                return tt==TokenType.Operator;
+                return tt == TokenType.Operator;
             }
             else
             {
                 return false;
             }
         }
+
         public Token ParseToken()
         {
             var result = "";
@@ -156,23 +217,25 @@ namespace Calculator
                     result += this.CurrentCharater;
                     this.NextToken();
                 }
-                return new Token(TokenType.Number, result);
-            }else if (this.Is(TokenType.Operator))
+
+                return new NumberToken(result);
+            }
+            else if (this.Is(TokenType.Operator))
             {
-                var op=this.CurrentCharater;
+                var op = this.CurrentCharater;
                 this.NextToken();
-                return new Token(TokenType.Operator, op.ToString());
-            } else if (this.CurrentIndex >= this.Input.Length)
+                return new OperatorToken(op.ToString());
+            }
+            else if (this.CurrentIndex >= this.Input.Length)
             {
-                return new Token(TokenType.EOF, "\0");
+                return new EOFToken();
             }
             else
             {
-                var literal=this.CurrentCharater;
+                var literal = this.CurrentCharater;
                 this.NextToken();
-                return new Token(TokenType.Invalid, literal.ToString());
+                return new InvalidToken(literal.ToString());
             }
-            
         }
 
         public List<Token> ParseAll()
@@ -180,9 +243,9 @@ namespace Calculator
             // Reset
             this.CurrentIndex = -1;
             this.NextToken();
-            var tokens=new List<Token>();
+            var tokens = new List<Token>();
             tokens.Add(this.ParseToken());
-            while (tokens[^1].Type!=TokenType.EOF)
+            while (tokens[^1].Type != TokenType.EOF)
             {
                 tokens.Add(this.ParseToken());
             }
@@ -191,8 +254,9 @@ namespace Calculator
         }
     }
 
-    public class Notation
+    public interface Notation
     {
+        double Eval();
     }
 
     public class Number : Notation
@@ -203,8 +267,19 @@ namespace Calculator
         {
             this.Value = value;
         }
+
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+
+        public double Eval()
+        {
+            return this.Value;
+        }
     }
-    public class Inflix:Notation
+
+    public class Inflix : Notation
     {
         public Notation Lhs { get; set; }
         public Operators Op { get; set; }
@@ -216,16 +291,38 @@ namespace Calculator
             this.Op = op;
             this.Rhs = rhs;
         }
+
         public override string ToString()
         {
-            return "("+Lhs+" "+Op+" "+Rhs+")";
+            return "(" + this.Lhs + " " + this.Op + " " + this.Rhs + ")";
+        }
+
+        public double Eval()
+        {
+            switch (this.Op)
+            {
+                case (Operators.Plus):
+                    return this.Lhs.Eval() + this.Rhs.Eval();
+                case (Operators.Minus):
+                    return this.Lhs.Eval() - this.Rhs.Eval();
+                case (Operators.Multiply):
+                    return this.Lhs.Eval() * this.Rhs.Eval();
+                case (Operators.Divide):
+                    return this.Lhs.Eval() / this.Rhs.Eval();
+                case (Operators.Power):
+                    return Math.Pow((double)this.Lhs.Eval(), (double)this.Rhs.Eval());
+                default:
+                    throw new NotSupportedException($"Operator {this.Op} is not supported");
+            }
         }
     }
+
     public class Parser // FIXME: A lot of duplicate code
     {
-        public List<Token> Tokens{get; private set;}
-        public int CurrentIndex {get; private set;}
-        public Token CurrentToken {get; private set;}
+        public List<Token> Tokens { get; private set; }
+        public int CurrentIndex { get; private set; }
+        public Token CurrentToken { get; private set; }
+
         public Parser(List<Token> tokens)
         {
             this.Tokens = tokens;
@@ -239,12 +336,13 @@ namespace Calculator
             CurrentIndex = -1;
             this.NextToken();
         }
+
         public void NextToken()
         {
-            if (this.CurrentIndex+1 < this.Tokens.Count)
+            if (this.CurrentIndex + 1 < this.Tokens.Count)
             {
                 this.CurrentIndex++;
-                this.CurrentToken=this.Tokens[this.CurrentIndex];
+                this.CurrentToken = this.Tokens[this.CurrentIndex];
             }
             else
             {
@@ -257,39 +355,50 @@ namespace Calculator
             if (this.CurrentIndex + 1 < this.Tokens.Count)
             {
                 return this.Tokens[this.CurrentIndex + 1];
-            }else
+            }
+            else
             {
                 throw new Exception("Unexpected end of file"); // Peek should not be called when Token is already EOF
             }
         }
+
         public bool Is(TokenType t)
         {
             return CurrentToken.Type == t;
         }
 
-        public Notation ParseToken(Notation lhs, int minPrecedence=0) // https://en.wikipedia.org/wiki/Operator-precedence_parser
+        public Notation
+            ParseToken(Notation lhs, int minPrecedence = 0) // https://en.wikipedia.org/wiki/Operator-precedence_parser
         {
-           while (this.Peek().Type==TokenType.Operator&&this.Peek().GetPrecedence() >= minPrecedence)
-           {
-               this.NextToken();
-               Console.WriteLine(this.CurrentToken);
-               var op=this.CurrentToken;
-               this.NextToken();
-               Debug.Assert(this.CurrentToken.Type==TokenType.Number);
-               Notation rhs=new Number(int.Parse(this.CurrentToken.Literal));
-               while (this.Peek().Type==TokenType.Operator&&this.Peek().GetPrecedence()>op.GetPrecedence())
-               {
-                   rhs=this.ParseToken(rhs,(int)op.GetPrecedence().Value+(this.Peek().GetPrecedence().Value>op.GetPrecedence().Value?1:0));
-                   //this.NextToken();
-               }
+            while (this.Peek().Type == TokenType.Operator && ((OperatorToken)this.Peek()).Precedence >= minPrecedence)
+            {
+                var op = (OperatorToken)this.Peek();
+                this.NextToken();
+                this.NextToken();
+                Debug.Assert(this.CurrentToken.Type == TokenType.Number);
+                Notation rhs = new Number(int.Parse(this.CurrentToken.Literal));
+                while (this.Peek().Type == TokenType.Operator &&
+                       ((((OperatorToken)this.Peek()).Precedence > op.Precedence) ||
+                        (((OperatorToken)this.Peek()).Precedence == op.Precedence &&
+                         ((OperatorToken)this.Peek()).Associative == Direction.Right)))
+                {
+                    var peekOp = (OperatorToken)this.Peek();
+                    rhs = this.ParseToken(rhs,
+                        (int)op.Precedence +
+                        (peekOp.Precedence > op.Precedence ? 1 : 0));
+                }
 
-               return new Inflix(lhs, op.GetOperator().Value, rhs);
-           }
-           throw new Exception("Unexpected token");
+                lhs = new Inflix(lhs, op.Operator, rhs);
+            }
+
+            Console.WriteLine(lhs);
+            return lhs;
         }
 
         public Notation Parse()
         {
+            this.CurrentIndex = -1; //reset
+            this.NextToken();
             if (this.CurrentToken.Type != TokenType.Number)
             {
                 throw new Exception("Notation have to start with a number");
@@ -297,13 +406,13 @@ namespace Calculator
 
             return this.ParseToken(new Number(int.Parse(this.CurrentToken.Literal)));
         }
-        
     }
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            var lexer = new Lexer("100+2*3");
+            var lexer = new Lexer("2*5+2*4^3");
             var tokens = lexer.ParseAll();
             for (var i = 0; i < tokens.Count; i++)
             {
@@ -311,8 +420,9 @@ namespace Calculator
             }
 
             var parser = new Parser(lexer);
-            Console.WriteLine(parser.Parse());
+            var result = parser.Parse();
+            Console.WriteLine(result);
+            Console.WriteLine(result.Eval());
         }
     }
 }
-

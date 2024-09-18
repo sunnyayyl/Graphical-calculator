49608a53e1e10e568c51719271eaa428e1ce4157
sunny.ayyl <sunny.ayyl@gmail.com>
2024-09-18 17:55:29 +0100

    UI setup

---
+++ UI/Form1.cs
@@ -0,0 +1,21 @@
+namespace UI;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+    {
+    }
+
+    private void button1_Click_1(object sender, EventArgs e)
+    {
+    }
+}

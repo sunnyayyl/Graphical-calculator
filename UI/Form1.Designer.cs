0289f936862011eb18db8043f537466ccbde4564
sunny.ayyl <sunny.ayyl@gmail.com>
2024-10-18 22:58:29 +0100

    Separate graph into its own control

--- UI/Form1.Designer.cs
+++ UI/Form1.Designer.cs
@@ -30,9 +30,8 @@ partial class Form1
     private void InitializeComponent()
     {
         textBox1 = new TextBox();
-        pictureBox1 = new PictureBox();
         tableLayoutPanel1 = new TableLayoutPanel();
-        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+        graphControl1 = new GraphControl();
         tableLayoutPanel1.SuspendLayout();
         SuspendLayout();
         // 
@@ -44,24 +43,13 @@ partial class Form1
         textBox1.TabIndex = 0;
         textBox1.TextChanged += textBox1_TextChanged;
         // 
-        // pictureBox1
-        // 
-        pictureBox1.Dock = DockStyle.Fill;
-        pictureBox1.Location = new Point(120, 3);
-        pictureBox1.Name = "pictureBox1";
-        pictureBox1.Size = new Size(677, 444);
-        pictureBox1.TabIndex = 1;
-        pictureBox1.TabStop = false;
-        pictureBox1.Paint += pictureBox1_Paint;
-        pictureBox1.MouseMove += pictureBox1_MouseMove;
-        // 
         // tableLayoutPanel1
         // 
         tableLayoutPanel1.ColumnCount = 2;
         tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 14.6907215F));
         tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 85.30928F));
         tableLayoutPanel1.Controls.Add(textBox1, 0, 0);
-        tableLayoutPanel1.Controls.Add(pictureBox1, 1, 0);
+        tableLayoutPanel1.Controls.Add(graphControl1, 1, 0);
         tableLayoutPanel1.Dock = DockStyle.Fill;
         tableLayoutPanel1.Location = new Point(0, 0);
         tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -69,6 +57,15 @@ partial class Form1
         tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
         tableLayoutPanel1.Size = new Size(800, 450);
         tableLayoutPanel1.TabIndex = 2;
+        // 
+        // graphControl1
+        // 
+        graphControl1.BackColor = Color.White;
+        graphControl1.Dock = DockStyle.Fill;
+        graphControl1.Location = new Point(120, 3);
+        graphControl1.Name = "graphControl1";
+        graphControl1.Size = new Size(677, 444);
+        graphControl1.TabIndex = 1;
         // 
         // Form1
         // 
@@ -78,9 +75,6 @@ partial class Form1
         Controls.Add(tableLayoutPanel1);
         Name = "Form1";
         Text = "Form1";
-        Load += Form1_Load;
-        ResizeEnd += Form1_ResizeEnd;
-        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
         tableLayoutPanel1.ResumeLayout(false);
         tableLayoutPanel1.PerformLayout();
         ResumeLayout(false);
@@ -89,6 +83,6 @@ partial class Form1
     #endregion
 
     private TextBox textBox1;
-    private PictureBox pictureBox1;
     private TableLayoutPanel tableLayoutPanel1;
+    private GraphControl graphControl1;
 }
--- UI/Form1.cs
+++ UI/Form1.cs
@@ -5,14 +5,6 @@ namespace UI;
 
 public partial class Form1 : Form
 {
-    private int? _mouseOldX, _mouseOldY;
-    private float _xScale = 1.0f;
-    private float _yScale = 1.0f;
-    private float _xOffset = 0.0f;
-    private float _yOffset = 0.0f;
-    private IExpression? _expression;
-    private bool _hasError;
-
     public Form1()
     {
         InitializeComponent();
@@ -21,144 +13,33 @@ public partial class Form1 : Form
     private void ReParse()
     {
         var stopwatch = new Stopwatch();
-        this._hasError = false;
-        this._expression = null;
+        IExpression? expression = null;
         try
         {
             stopwatch.Start();
             var l = new Lexer(textBox1.Text.Trim());
             var p = new Parser(l);
-            this._expression = p.Parse();
+            expression = p.Parse();
             stopwatch.Stop();
             Debug.WriteLine("Successfully parsed expression in " + stopwatch.ElapsedMilliseconds + "ms");
         }
         catch (Exception ex) when (ex is IErrorMessage)
         {
             Debug.WriteLine(ex.Message);
-            this._hasError = true;
+            this.graphControl1.MarkError();
         }
+
+        if (expression != null)
+        {
+            this.graphControl1.ClearExpressions();
+            this.graphControl1.AddExpression(expression);
+        }
+
+        this.graphControl1.Refresh();
     }
 
     private void textBox1_TextChanged(object sender, EventArgs e)
     {
         this.ReParse();
-        this.pictureBox1.Refresh();
-    }
-
-    private void pictureBox1_Paint(object sender, PaintEventArgs e)
-    {
-        PointF? last = null;
-        var size = this.pictureBox1.Size;
-        var width = size.Width;
-        var height = size.Height;
-        var halfWidth = (float)width / 2 + this._xOffset;
-        var halfHeight = (float)height / 2 + this._yOffset;
-        var pen = new Pen(Color.Red);
-        var input = new Dictionary<char, double>
-        {
-            ['x'] = 0
-        };
-        if (this._expression is { } expression)
-        {
-            for (var x = -halfWidth; x <= width - halfWidth; x += 0.1f)
-            {
-                input['x'] = x;
-                float yRaw;
-                try
-                {
-                    yRaw = (float)expression.Eval(input);
-                }
-                catch (Exception ex) when (ex is IErrorMessage)
-                {
-                    this._hasError = true;
-                    Console.WriteLine(ex.Message);
-                    break;
-                }
-
-                var plotX = (x + halfWidth) * this._xScale;
-                var plotY = (-yRaw + halfHeight) * this._yScale;
-                if (Math.Abs(plotX) > Width || Math.Abs(plotY) > Height)
-                {
-                    last = null;
-                    continue;
-                }
-
-                var current = new PointF(plotX, plotY);
-                if (last.HasValue)
-                {
-                    e.Graphics.DrawLine(pen, last.Value, current);
-                }
-
-                last = current;
-            }
-        }
-
-        if (this._hasError)
-        {
-            this.pictureBox1.BackColor = Color.Pink;
-        }
-        else
-        {
-            this.pictureBox1.BackColor = Color.White;
-        }
-
-        //var maxY = Math.Clamp(halfHeight * this._yScale, 0, height);
-        e.Graphics.DrawLine(new Pen(Color.Black), new PointF(0, halfHeight * this._yScale),
-            new PointF(width, halfHeight * this._yScale));
-        e.Graphics.DrawLine(new Pen(Color.Black), new PointF(halfWidth * this._xScale, 0),
-            new PointF(halfWidth * this._xScale, height));
-    }
-
-    private void Form1_Load(object sender, EventArgs e)
-    {
-    }
-
-    private void Form1_ResizeEnd(object sender, EventArgs e)
-    {
-        this.pictureBox1.Refresh();
-    }
-
-    protected override void WndProc(ref Message m)
-    {
-        if (m.Msg == 0x0112)
-        {
-            this.pictureBox1.Invalidate();
-        }
-
-        base.WndProc(ref m);
-    }
-
-    private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
-    {
-        if (Control.MouseButtons == MouseButtons.Left)
-        {
-            if (_mouseOldX.HasValue && _mouseOldY.HasValue)
-            {
-                this._xOffset +=
-                    ((float)e.X - _mouseOldX.Value) * (1 / this._xScale); // Change sensitivity based on scale
-                this._yOffset += ((float)e.Y - _mouseOldY.Value) * (1 / this._xScale);
-            }
-
-            _mouseOldX = e.X;
-            _mouseOldY = e.Y;
-            this.pictureBox1.Refresh();
-        }
-        else
-        {
-            _mouseOldX = null;
-            _mouseOldY = null;
-        }
-    }
-
-    protected override void OnMouseWheel(MouseEventArgs e)
-    {
-        this._xScale =
-            Math.Clamp(this._xScale + e.Delta * 0.001f, float.Epsilon,
-                float.PositiveInfinity); // Prevent scale from going to 0 and below
-        this._yScale = Math.Clamp(this._yScale + e.Delta * 0.001f, float.Epsilon, float.PositiveInfinity);
-        Debug.WriteLine($"Scale: {this._xScale}, {this._yScale}");
-        var mouseX = (float)e.X;
-        this.pictureBox1.Refresh();
-        base.OnMouseWheel(e);
     }
 }
